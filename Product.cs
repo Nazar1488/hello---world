daa22bbbb48db0958689b5951955e937501d55a4
IvanPilyak <38115912+IvanPilyak@users.noreply.github.com>
2018-04-12 19:17:49 +0300

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace product_project_practic
+{
+    class Product 
+    {
+        private string name;
+        private int price;
+        private int number;
+        public Product(string _name, int _price, int _number)
+        {
+            name = _name;
+            Price = _price;
+            Number = _number;
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+           private  set
+            {
+                if(value<0)
+                {
+                    price = 0;
+                }
+                else
+                {
+                    price = value;
+                }
+            }
+        }
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            private set
+            {
+                if(value<0)
+                {
+                    number = 0;
+                }
+                else
+                {
+                    number = value;
+                }
+            }
+        }
+        public int sum_price()
+        {
+            return price * number;
+        }
+       
+        public override string ToString()
+        {
+            return "name: "+ name + "\t"+"price: " + price+ "\t"+ "number: " + "\t" + number;
+        }
+
+
+    }
+    class ProductNameComperer : IComparer<Product>
+    {
+        public int Compare(Product first,Product second)
+        {
+            return string.Compare(first.Name ,second.Name);
+             
+        }
+    }
+    class ProductPriceComperer : IComparer<Product>
+    {
+        public int Compare(Product first, Product second)
+        {
+            if (first.Price > second.Price) return 1;
+            else if (first.Price < second.Price) return -1;
+            else return 0;
+
+
+        }
+    }
+    class ProductNumberComperer : IComparer<Product>
+    {
+        public int Compare(Product first, Product second)
+        {
+            if (first.Number > second.Number) return 1;
+            else if (first.Number < second.Number) return -1;
+            else return 0;
+
+
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace product_project_practic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1.Read from file");
+                Console.WriteLine("2.Sort by name");
+                Console.WriteLine("3.Sort by price");
+                Console.WriteLine("4.Sort by number");
+                Console.WriteLine("5.Find by name");
+                Console.WriteLine("6.Show all products");
+                Console.WriteLine("7.Show all products with sum price");
+                Console.WriteLine("8.Exit");
+                string key= Console.ReadLine();
+                if (key == "1")
+                {
+                    products.Clear();
+                    StreamReader fs = new StreamReader(@"..\\..\\text.txt");
+                    while (!fs.EndOfStream)
+                    {
+                        string temp1 = fs.ReadLine();
+                        if (temp1 != null && temp1 !="")
+                        {
+                            string[] res = temp1.Split();
+                            products.Add(new Product(res[0], Convert.ToInt32(res[1]), Convert.ToInt32(res[2])));
+                        }
+                    }
+                    foreach (Product p in products)
+                    {
+                        Console.WriteLine(p);
+                    }
+                }
+                if(key=="2")
+                {
+                    products.Sort( new ProductNameComperer());
+                    foreach (Product p in products)
+                    {
+                        Console.WriteLine(p);
+                    }
+                }
+                if (key == "3")
+                {
+                    products.Sort(new ProductPriceComperer());
+                    foreach (Product p in products)
+                    {
+                        Console.WriteLine(p);
+                    }
+                }
+                if (key == "4")
+                {
+                    products.Sort(new ProductNumberComperer());
+                    foreach (Product p in products)
+                    {
+                        Console.WriteLine(p);
+                    }
+                }
+                if(key =="5")
+                {
+                    Console.Write("name=");
+                    string val = Console.ReadLine();
+                    bool flag = false;
+                    foreach(Product p in products)
+                    {
+                        if(p.Name == val)
+                        {
+                            flag = true;
+                            Console.WriteLine(p);
+                        }
+                    }
+                    if(flag==false)
+                    {
+                        Console.WriteLine("Products with this name are not!");
+                    }
+                }
+                if(key=="6")
+                {
+                    foreach(Product p in products)
+                    {
+                        Console.WriteLine(p);
+                    }
+                }
+                if(key=="7")
+                {
+                    foreach(Product p in products)
+                    {
+                        Console.WriteLine(p +"\t"+ "sum_price: " + p.sum_price());
+                    }
+                }
+                if(key=="8")
+                {
+                    break;
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
